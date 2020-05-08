a70d18b216dd802e01654b69b236a4d228896c83
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-08 22:47:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,9 +17,9 @@ def add_element_to_start_of_array(array, element)
    array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array, element)
+def remove_element_from_end_of_array(array)
   array = []
-  array.pop(element)
+  array[-1].pop
  
 end
 
